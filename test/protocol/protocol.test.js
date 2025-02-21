bc196154dc5e7161fbaf9710c7603719c70eb97c
yeahnotsewerside <dreenot@gmail.com>
2025-02-21 15:24:45 +0200

    prettify

--- test/protocol/protocol.test.js
+++ test/protocol/protocol.test.js
@@ -8,7 +8,7 @@ import { ThorWallet } from '../../../src/wallet'
 import { pollReceipt } from '../../../src/transactions'
 
 function nextValidator(rawBlock) {
-    return ""
+    return ''
 }
 
 describe('POST /accounts/*', function () {
@@ -21,28 +21,33 @@ describe('POST /accounts/*', function () {
         fundedWallet = await ThorWallet.newFunded({ vet: '0x0', vtho: 1e18 })
         await fundedWallet.waitForFunding()
 
-        bestBlockNumber = (await Client.raw.getBlock("best", false, false)).body?.number
+        bestBlockNumber = (await Client.raw.getBlock('best', false, false)).body
+            ?.number
     })
 
     it.e2eTest('validate proposers', 'all', async () => {
-        const prevBlock = await Client.raw.getBlock(bestBlockNumber - 1, false, true)
+        const prevBlock = await Client.raw.getBlock(
+            bestBlockNumber - 1,
+            false,
+            true,
+        )
 
-        expect(
-            block.success,
-            'API response should be a success',
-        ).toBeTruthy()
+        expect(block.success, 'API response should be a success').toBeTruthy()
         expect(block.httpCode, 'Expected HTTP Code').toEqual(200)
 
-        const bestBlock = await Client.raw.getBlock(bestBlockNumber, false, false)
+        const bestBlock = await Client.raw.getBlock(
+            bestBlockNumber,
+            false,
+            false,
+        )
 
-        expect(
-            block.success,
-            'API response should be a success',
-        ).toBeTruthy()
+        expect(block.success, 'API response should be a success').toBeTruthy()
         expect(block.httpCode, 'Expected HTTP Code').toEqual(200)
 
         validator = nextValidator(prevBlock.body)
 
-        expect(bestBlock.body.proposer, 'Proposer should be equal').toEqual(validator)
+        expect(bestBlock.body.proposer, 'Proposer should be equal').toEqual(
+            validator,
+        )
     })
 })
