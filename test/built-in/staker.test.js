8216966ef8787bc96fc9818322b06830f7007ea2
Darren Kelly <darren.kelly@vechain.org>
2025-03-06 11:09:36 +0000

    fix: staker test

--- test/built-in/staker.test.js
+++ test/built-in/staker.test.js
@@ -2,6 +2,7 @@ import { Client } from '../../src/thor-client'
 import { contractAddresses } from '../../src/contracts/addresses'
 import { interfaces } from '../../src/contracts/hardhat'
 import { ThorWallet } from '../../src/wallet'
+import { randomAddress } from 'hardhat/internal/hardhat-network/provider/utils/random'
 
 describe('POST /accounts/*', function () {
     const wallet = ThorWallet.withFunds()
@@ -14,30 +15,27 @@ describe('POST /accounts/*', function () {
     })
 
     it.e2eTest('should get non-existing validator', 'all', async () => {
+        const addr = await randomAddress()
+
         const res = await Client.raw.executeAccountBatch({
             clauses: [
                 {
                     to: contractAddresses.staker,
                     value: '0x0',
                     data: interfaces.staker.encodeFunctionData('get', [
-                        wallet.address,
+                        addr.toString()
                     ]),
                 },
             ],
-            caller: wallet.address,
+            caller: addr.toString()
         })
         expect(res.success, 'API response should be a success').toBeTruthy()
         expect(res.httpCode, 'Expected HTTP Code').toEqual(200)
-        expect(res.body, 'Expected Response Body').toEqual([
-            {
-                data: '0x',
-                events: [],
-                transfers: [],
-                gasUsed: expect.any(Number),
-                reverted: false,
-                vmError: '',
-            },
-        ])
+
+        const [stake, weight, status] = interfaces.staker.decodeFunctionResult("get", res.body[0].data)
+        expect(stake).toEqual(0n)
+        expect(weight).toEqual(0n)
+        expect(status).toEqual(0n)
     })
 
     it.e2eTest('should add validators', 'all', async () => {})
