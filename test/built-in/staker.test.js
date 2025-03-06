725f9f82a1fd8fffcda85ff004583c670b62aaaf
Darren Kelly <darren.kelly@vechain.org>
2025-03-06 10:51:46 +0000

    fix: fix staker paths

--- test/built-in/staker.test.js
+++ test/built-in/staker.test.js
@@ -1,11 +1,7 @@
-import { Client } from '../../../src/thor-client'
-import { contractAddresses } from '../../../src/contracts/addresses'
-import { Staker__factory } from '../../../typechain-types'
-import { interfaces } from '../../../src/contracts/hardhat'
-import { getBlockRef } from '../../../src/utils/block-utils'
-import { revisions } from '../../../src/constants'
-import { ThorWallet } from '../../../src/wallet'
-import { pollReceipt } from '../../../src/transactions'
+import { Client } from '../../src/thor-client'
+import { contractAddresses } from '../../src/contracts/addresses'
+import { interfaces } from '../../src/contracts/hardhat'
+import { ThorWallet } from '../../src/wallet'
 
 describe('POST /accounts/*', function () {
     const wallet = ThorWallet.withFunds()
