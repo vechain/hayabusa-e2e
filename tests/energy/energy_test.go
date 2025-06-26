2f4084ea7238d97b339fefe7bc2eebd2a29917a6
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-06-26 13:49:07 +0100

    added polling before connecting peers

--- tests/energy/energy_test.go
+++ tests/energy/energy_test.go
@@ -37,8 +37,6 @@ func TestEnergy(t *testing.T) {
 	energy, err := builtin.NewEnergy(client)
 	require.NoError(t, err)
 
-	energy.Raw().Client().AccountStorage()
-
 	require.NoError(t, utils.WaitForFork(staker, config.ForkBlock))
 
 	senders := &utils.Senders{}
