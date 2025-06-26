96760743f05d8ac2bfe3ef61332562c9d60f1d49
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-06-26 17:00:41 +0100

    using 0.0.0.0

--- hayabusa/hayabusa.go
+++ hayabusa/hayabusa.go
@@ -130,7 +130,7 @@ func StartNetwork(t *testing.T, config *Config) (*thorclient.Client, environment
 			Genesis:        customGenesis,
 			Verbosity:      verbosity,
 			AdditionalArgs: additionalArgs,
-			APIAddr:        fmt.Sprintf("127.0.0.1:%d", apiPort),
+			APIAddr:        fmt.Sprintf("0.0.0.0:%d", apiPort),
 			P2PListenPort:  p2pPort,
 		}
 	}
@@ -316,7 +316,7 @@ func cleanupPorts(ports []int) {
 
 // isPortAvailable checks if a port is actually available for binding
 func isPortAvailable(port int) bool {
-	addr := fmt.Sprintf("127.0.0.1:%d", port)
+	addr := fmt.Sprintf("0.0.0.0:%d", port)
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
 		return false
