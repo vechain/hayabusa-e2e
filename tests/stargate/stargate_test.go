1e38385518e706ba545f0400e9608f33ffb0d4cf
Miguel Angel Rojo Fernandez <miguel.rojo@vechain.org>
2025-06-27 15:18:17 +0100

    refactor

--- tests/stargate/stargate_test.go
+++ tests/stargate/stargate_test.go
@@ -36,13 +36,14 @@ func Test_Stargate_SingleDelegator(t *testing.T) {
 	// wait for the validator to complete 1 staking period
 	block := config.ForkBlock + config.TransitionPeriod + config.MinStakingPeriod
 	require.NoError(t, ticker.WaitForBlock(block))
-	ticker.WaitForCondition(time.Minute*5, func() (bool, error) {
+	err = ticker.WaitForCondition(time.Minute*5, func() (bool, error) {
 		completed, err := staker.GetCompletedPeriods(validationID)
 		if err != nil {
 			return false, err
 		}
 		return *completed == 1, nil
 	})
+	require.NoError(t, err)
 	completed, err := staker.GetCompletedPeriods(validationID)
 	require.NoError(t, err)
 	assert.Equal(t, 1, int(*completed))
