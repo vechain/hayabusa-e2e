618be7001837fbf874e052a3cf1f8dbe4d836124
tony <liboliqi@gmail.com>
2025-07-31 21:38:45 +0800

    update stargate contract

--- hayabusa/stargate/gen.go
+++ hayabusa/stargate/gen.go
@@ -25,5 +25,5 @@ package stargate
 //go:generate sh -c "cp -r openzeppelin-contracts-upgradeable-5.0.0/contracts/* contracts/openzeppelin-upgradeable"
 //go:generate sh -c "rm -rf openzeppelin.zip openzeppelin-contracts-5.0.0 openzeppelin-upgradeable.zip openzeppelin-contracts-upgradeable-5.0.0"
 
-//go:generate solc @openzeppelin/contracts-upgradeable=contracts/openzeppelin-upgradeable @openzeppelin/contracts=contracts/openzeppelin/  --evm-version paris --overwrite --optimize --via-ir --optimize-runs 200 -o ./ --abi --bin contracts/StargateDelegation.sol stargate.sol
+//go:generate sh -c "docker run -v ./:/sources ethereum/solc:0.8.20 @openzeppelin/contracts-upgradeable=/sources/contracts/openzeppelin-upgradeable @openzeppelin/contracts=/sources/contracts/openzeppelin/  --evm-version paris --overwrite --optimize --via-ir --optimize-runs 200 -o /sources/ --abi --bin /sources/contracts/StargateDelegation.sol /sources/stargate.sol"
 //go:generate sh -c "rm -rf IERC20* IStaker* contracts interfaces StargateNFT A*.abi A*.bin C*.abi C*.bin c*.abi c*.bin D*.abi D*.bin E*.abi E*.bin I*.abi I*.bin M*.abi M*.bin R*.abi R*.bin Sa*.abi Sa*.bin Sto*.abi Sto*.bin sta*.abi sta*.bin T*.abi T*.bin U*.abi U*.bin"
