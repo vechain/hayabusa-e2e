456090d5865d3a975bed2233da2d36e99e431b94
Darren Kelly <darren.kelly@vechain.org>
2025-08-20 10:00:28 +0100

    fix: small revert

--- cmd/gasusage/main.go
+++ cmd/gasusage/main.go
@@ -78,7 +78,7 @@ func run(ctx context.Context) error {
 	}
 
 	tw := table.NewWriter()
-	tw.AppendHeader(table.Row{"Name", "Gas MainnetUsed"})
+	tw.AppendHeader(table.Row{"Name", "Gas Used"})
 	defer func() {
 		tw.SortBy([]table.SortBy{{Name: "Name", Mode: table.Asc}})
 		fmt.Println(tw.Render())
