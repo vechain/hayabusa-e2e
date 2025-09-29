c6b50dd058d25b0b6e7d6ac48d0d2e08eaaa5345
Darren Kelly <darren.kelly@vechain.org>
2025-09-29 19:03:11 +0100

    chore: tidy up contract

--- tests/balance/selfdestruct/SelfDestruct.sol
+++ tests/balance/selfdestruct/SelfDestruct.sol
@@ -4,16 +4,9 @@ pragma solidity ^0.8.0;
 contract SelfDestructible {
     constructor() payable {}
 
-    // Allow it to receive Ether after deployment as well
     receive() external payable {}
 
-    // Trigger selfdestruct — only the deployer can call this
     function destroy() external {
         selfdestruct(payable(address(0x00000000000000000000000000005374616B6572)));
-    }
-
-    // Helper to check the contract balance
-    function getBalance() external view returns (uint) {
-        return address(this).balance;
     }
 }
